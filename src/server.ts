d458e7d9d70b57785a6be3834ba357b5ce18908d
Suhag Al Amin <developersuhag@gmail.com>
2023-06-04 15:54:15 +0600

    logger setup with rotate rules

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import { errorLogger, logger } from './shared/logger'
 
 const main = async () => {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('Database connected')
+    logger.info('Database connected')
     app.listen(config.port, () => {
-      console.log(`University management app listening on port ${config.port}`)
+      logger.info(`University management app listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log('Failed to connecte', err)
+    errorLogger.error('Failed to connecte', err)
   }
 }
 main()
